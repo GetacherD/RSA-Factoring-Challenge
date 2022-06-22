ab57720fcb6eb1e8755843af73ddae2c2e3a526a
Getacher <Getacher.d@gmail.com>
2022-06-22 14:42:26 +0300

    test

---
+++ factors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import math
+import sys
+
+
+def is_prime(N):
+    if N % 2 == 0:
+        return False
+    for i in range(3, round(math.sqrt(N)) + 1, 2):
+        if N % i == 0:
+            return False
+    return True
+
+
+def factor(N):
+
+    if N % 2 == 0:
+        print("{}={}x{}".format(N, N // 2, 2))
+    else:
+        for i in range(3, round(math.sqrt(N)) + 1, 2):
+            if N % i == 0:
+                print("{}={}x{}".format(N, N // i, i))
+                break
+
+
+with open(sys.argv[1]) as f:
+    for line in f.readlines():
+        factor(int(line))
